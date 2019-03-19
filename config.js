82280dc3887cacb0d1247185b68be7986feb127f
Danofares <45193464+Danofares@users.noreply.github.com>
2019-03-19 11:55:28 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+exports.username = 'DnDBot';
+exports.password = '';
+
+exports.commandToken = '#';
+
+exports.host = 'sim.psim.us';
+exports.port = '8000';
+
+exports.serverhost = 'localhost';
+exports.serverport = '8000';
+
+exports.avatar = 0;
+
+exports.rooms = [];
+
+exports.owners = ['danofares'];
